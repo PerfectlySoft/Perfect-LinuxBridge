af6d8ad7de973e2c6250eb0a906596dba7a91cb3
Kyle Jessup <badthing@gmail.com>
2016-05-26 09:00:43 -0400

    LinuxBridge funcs moved to header and extern inlined

--- LinuxBridge.h
+++ LinuxBridge.h
@@ -10,16 +10,45 @@
 #include <sys/epoll.h>
 #include <sys/timerfd.h>
 #include <signal.h>
+#include <errno.h>
+#include <sys/time.h>
 
 #undef SIG_IGN
-extern __sighandler_t SIG_IGN;
+__sighandler_t SIG_IGN = (__sighandler_t)1;
 
-int linux_open(const char *path, int oflag, mode_t mode);
-int linux_errno();
-int linux_fcntl_get(int fd, int cmd);
-int linux_fcntl_set(int fd, int cmd, int value);
-unsigned long long htonll(unsigned long long host_longlong);
-unsigned long long ntohll(unsigned long long host_longlong);
-int pthread_cond_timedwait_relative_np(void * cond, void * mutx, const struct timespec * tmspec);
+extern inline int linux_fcntl_get(int fd, int cmd)
+{
+	return fcntl(fd, cmd);
+}
+
+extern inline int linux_fcntl_set(int fd, int cmd, int value)
+{
+	return fcntl(fd, cmd, value);
+}
+
+extern inline int linux_open(const char *path, int oflag, mode_t mode)
+{
+	return open(path, oflag, mode);
+}
+
+extern inline int linux_errno()
+{
+	return errno;
+}
+
+extern inline int pthread_cond_timedwait_relative_np(pthread_cond_t * cond, pthread_mutex_t * mutx, const struct timespec * tmspec)
+{
+	struct timeval time;
+	struct timespec timeout;
+	gettimeofday(&time, NULL);
+	timeout.tv_sec = time.tv_sec + tmspec->tv_sec;
+	timeout.tv_nsec = (time.tv_usec * 1000) + tmspec->tv_nsec;
+	
+	timeout.tv_sec += timeout.tv_nsec / 1000000000;
+	timeout.tv_nsec %= 1000000000;
+	
+	int i = pthread_cond_timedwait(cond, mutx, &timeout);
+	return i;
+}
 
 #endif
